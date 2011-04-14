c35b7dd3bc7f6622de62ee3a6c9249c100f66dba
Jérémy Subtil <jeremy.subtil@smile.fr>
2011-04-14 11:07:19 +0200

    Bootstrapped plugin.

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_create_related_issue do
+  name 'Redmine Create Related Issue plugin'
+  author 'Smile'
+  description 'This plugin for Redmine allows you to directly create a related issue from another one.'
+  version '0.0.1'
+  url 'https://github.com/smile-oss/redmine_create_related_issue'
+  author_url 'http://www.smile.fr/en/'
+end
